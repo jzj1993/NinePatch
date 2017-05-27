b76ae3b43c6836058ee7982f29461e93e91bb875
jzj <jiangzijian@meituan.com>
2017-05-27 16:57:07 +0800

    initial commit

---
+++ app/src/main/java/com/jzj/ninepatch/NinePatchActivity.java
@@ -0,0 +1,13 @@
+package com.jzj.ninepatch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NinePatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nine_patch_activity);
+    }
+}
